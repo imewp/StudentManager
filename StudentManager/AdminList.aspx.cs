e33f3370ebd4d9d59f845a2383d1276374cd9a35
Always_s <1477243047@qq.com>
2019-12-26 14:37:30 +0800

    管理员列表

--- StudentManager/AdminList.aspx.cs
+++ StudentManager/AdminList.aspx.cs
@@ -5,13 +5,47 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Model;
+using SQLDAL;
+
 namespace StudentManager
 {
     public partial class AdminList : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)//页面首次加载自动执行
+            {
+                LoadData();
+            }
         }
+
+        /// <summary>
+        /// 加载数据信息
+        /// </summary>
+        public void LoadData()
+        {
+            string conditon = string.Empty;
+            conditon = "(UserName is not null and UserName<>' ')";
+            if (!string.IsNullOrEmpty(user_name.Value))
+            {
+                conditon += " and UserName='" + user_name.Value + "'";
+            }
+            DALadmin_user dal = new DALadmin_user();
+            IList<admin_userEntity> admins = dal.Getadmin_usersbyCondition(conditon);//按照条件来查询数据
+            GridView1.DataSource = admins;
+            GridView1.DataBind();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminAdd.aspx");
+        }
+
     }
 }
